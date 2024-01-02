262b4a70533bc7fd51f46ff51f4151e46c5612fd
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-02 13:56:24 +0200

    Create 01-vars.js

---
+++ js/01-vars.js
@@ -0,0 +1,28 @@
+/*
+ * - Змінні і типи примітивів
+ * - const і let
+ * - Виведення даних у консоль методом console.log()
+ * - Унікальність індентифікатора
+ */
+
+const age = 10;
+const totalPrice = 200.64;
+const userName = 'Max';
+const message = 'Доброго вечора ми з України!';
+const isOpen = true;
+const shouldConfirm = false;
+
+console.log(age);
+console.log('Вартість:', totalPrice);
+console.log(userName);
+console.log(message);
+console.log(isOpen);
+console.log(shouldConfirm);
+
+/*
+ * * - Оператор typeof()
+ */
+
+/*
+ * - Як window.alert() блокує інтерпретацію
+ */
