99823ac79402321b4721bb4d6ee5548138dbc88a
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-03 12:18:16 +0200

    Update 04-string.js

--- js/04-string.js
+++ js/04-string.js
@@ -12,3 +12,36 @@ console.log(message.length);
 
 const firstName = 'Max';
 const lastName = 'Solovey';
+const fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+/*
+ * Напиши скрипт який виводить рядок у форматі:
+ * «Гість x y засиляється у n номер q»,
+ * підставив замість x y z q значення змінних
+ */
+
+const room = 716;
+const type = 'VIP';
+
+// This is old school
+/*
+const welcomeMsg = 'Гість ' + firstName + ' ' + lastName + ' засиляється у ' + type + ' номер ' + room;
+
+console.log(welcomeMsg);
+*/
+
+/*
+ * Шаблонні рядки (template strings)
+ * Повертаємось до стану рядка заселення у готель
+ */
+
+const quantity = 5;
+const orderMsg = `Ви замовляєте ${quantity} холодильників.`;
+
+console.log(orderMsg);
+
+const welcomeMsg = `Гість ${firstName} ${lastName} засиляється у ${type} номер ${room}`;
+
+console.log(welcomeMsg);
