39b98b6d777e1de5a077e1da4f02446b3dd62907
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-03 12:58:53 +0200

    Update 04-string.js

--- js/04-string.js
+++ js/04-string.js
@@ -50,15 +50,39 @@ console.log(welcomeMsg);
  * Нормалізація з методом toLowerCase()
  */
 
-const brand = 'Apple';
-const inDb = 'apple';
-const normalize = brand.toLowerCase();
+// let brand = prompt('Введіть бренд ноутбука');
+let brand = 'Apple';
+const normalizedBrand = brand.toLowerCase();
 
-console.log(normalize === inDb);
+console.log(normalizedBrand);
 
+/*
+* Нормалізація окремої частини рядку
+* за допомогою методу slice()
+*/
 
+let brandName = 'SamsUnG';
+console.log(brandName.slice(1).toLowerCase());
 
+brandName = brandName[0] + brandName.slice(1).toLowerCase();
+console.log(brandName);
 
 /*
  * Пошук у рядку з методом includes()
  */
+
+const blacklistedWord1 = 'спам';
+const blacklistedWord2 = 'розпродаж';
+
+const string1 = 'Доброго вечора ми з України, я Максим, це не спам, пропоную тобі приєднатись до нашої команди розробників!';
+const string2 = 'Найбільший РОЗПРОДАЖ цього тижня, не пропустіть!';
+const string3 = 'Рекламна компанія #fatlivesmatter';
+
+console.log(string1.includes(blacklistedWord1));
+console.log(string1.includes(blacklistedWord2));
+
+console.log(string2.includes(blacklistedWord1));
+console.log(string2.includes(blacklistedWord2));
+
+console.log(string3.includes(blacklistedWord1));
+console.log(string3.includes(blacklistedWord2));
