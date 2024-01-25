7e3e581ec27819a165178990730fe71403f3e9f3
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-25 18:14:14 +0200

    Update 01-3-iteration.js

--- src/js/01-3-iteration.js
+++ src/js/01-3-iteration.js
@@ -9,3 +9,30 @@ const feedback = {
 };
 
 let totalFeedback = 0;
+
+/*
+const keys = Object.keys(feedback);
+
+console.log(keys);
+
+for (const key of keys) {
+  console.log(key);
+  console.log(feedback[key]);
+
+  totalFeedback += feedback[key];
+}
+
+console.log('totalFeedback:', totalFeedback);
+*/
+
+const values = Object.values(feedback);
+
+console.log(values);
+
+for (const value of values) {
+  console.log(value);
+
+  totalFeedback += value;
+}
+
+console.log('totalFeedback:', totalFeedback);
