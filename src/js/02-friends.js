e713620297ba17ee15a90d9cfb53878a21697911
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-01-26 11:07:26 +0200

    Update 02-friends.js

--- src/js/02-friends.js
+++ src/js/02-friends.js
@@ -25,21 +25,33 @@ console.table(friends);
 
 const findFriendByName = function (allFriends, friendName) {
   for (const friend of allFriends) {
-    console.log(friend);
+    // console.log(friend);
+    // console.log(friend.name);
 
-    friendName = Object.values(friend);
-    console.log(friendName);
+    if (friend.name === friendName) {
+      return 'We found name!';
+    }
   }
+
+  return 'Not found name!';
 };
 
-console.log(findFriendByName(friends, 'Poly'));
-console.log(findFriendByName(friends, 'Chelsy'));
+// console.log(findFriendByName(friends, 'Poly'));
+// console.log(findFriendByName(friends, 'Chelsy'));
 
 /*
  * Отримуємо імена всіх друзів
  */
 
-const getAllNames = function (allFriends) {};
+const getAllNames = function (allFriends) {
+  const allNames = [];
+
+  for (const friend of allFriends) {
+    allNames.push(friend.name);
+  }
+
+  return allNames;
+};
 
 // console.log(getAllNames(friends));
 
@@ -47,6 +59,74 @@ const getAllNames = function (allFriends) {};
  * Отримуємо імена тільки друзів які онлайн
  */
 
-const getOnlineFriends = function (allFriends) {};
+const getOnlineFriends = function (allFriends) {
+  const onlineFriends = [];
+
+  for (const friend of allFriends) {
+    if (friend.online) {
+      onlineFriends.push(friend);
+    }
+  }
+
+  return onlineFriends;
+};
 
 // console.log(getOnlineFriends(friends));
+
+const getOfflineFriends = function (allFriends) {
+  const offlineFriends = [];
+
+  for (const friend of allFriends) {
+    if (!friend.online) {
+      offlineFriends.push(friend);
+    }
+  }
+
+  return offlineFriends;
+};
+
+// console.log(getOfflineFriends(friends));
+
+const getFriendsByOnlineStatus = function (allFriends) {
+  const status = { friendsOnline: [], friendsOffline: [] };
+
+  for (const friend of allFriends) {
+    if (friend.online) {
+      status.friendsOnline.push(friend);
+      continue;
+    }
+
+    status.friendsOffline.push(friend);
+
+    // ! Bad practice
+    // friend.online
+    //   ? status.friendsOnline.push(friend)
+    //   : status.friendsOffline.push(friend);
+
+    // * Good practice
+    // const key = friend.online ? 'friendsOnline' : 'friendsOffline';
+    // status[key].push(friend);
+  }
+
+  return status;
+};
+
+console.log(getFriendsByOnlineStatus(friends));
+
+/*
+const x = {
+  a: 1,
+  b: 2,
+  c: 50,
+  d: 100,
+};
+
+const sumKeys = function (keys) {
+  keys = Object.keys(x);
+  console.log(keys);
+
+  return keys.length;
+};
+
+console.log(sumKeys(x));
+*/
