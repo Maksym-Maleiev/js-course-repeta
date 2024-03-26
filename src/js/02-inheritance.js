753ec2fa2b6ca20d28d347a98ef8402c5cd292e1
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-26 10:44:52 +0200

    Update 02-inheritance.js

--- src/js/02-inheritance.js
+++ src/js/02-inheritance.js
@@ -17,12 +17,74 @@ class Hero {
 }
 
 class Warrior extends Hero {
-    constructor(config) {
-        super(config);
-        console.log(config);
+    constructor({ weapon, ...restProps } = {}) {
+        super(restProps);
+
+        this.weapon = weapon;
+    }
+
+    attack() {
+        console.log(`${this.name} атакує використовуючи ${this.weapon}`);
     }
 }
 
-const mango = new Warrior('mango', 1000);
+class Berserk extends Warrior {
+    constructor({ warcry, ...restProps } = {}) {
+        super(restProps);
 
-console.log(mango);
+        this.warcry = warcry;
+    }
+
+    babyRage() {
+        console.log(this.warcry);
+    }
+}
+
+class Mage extends Hero {
+    constructor({ spells, ...restProps } = {}) {
+        super(restProps);
+
+        this.spells = spells;
+    }
+
+    cast() {
+        console.log(`${this.name} щось там кастує 🧙‍♂️`);
+    }
+}
+
+const mango = new Warrior({ name: 'mango', xp: 1000, weapon: 'sword' });
+// console.log(mango);
+// mango.attack();
+// mango.gainXp(1000);
+
+const poly = new Mage({ name: 'poly', xp: 500, spells: ['фаєрбол'] });
+// console.log(poly);
+// poly.cast();
+// poly.gainXp(200);
+
+const ajax = new Berserk({
+    name: 'ajax',
+    xp: 500,
+    weapon: 'axe',
+    warcry: 'waaaaaaaah',
+});
+console.log(ajax);
+ajax.babyRage();
+ajax.attack();
+ajax.gainXp();
+
+// console.log(
+//     'mango.__proto__ === Warrior.prototype ',
+//     mango.__proto__ === Warrior.prototype,
+// );
+// // console.log(Object.getPrototypeOf(mango) === Warrior.prototype);
+
+// console.log(
+//     'Warrior.prototype.__proto__ === Hero.prototype ',
+//     Warrior.prototype.__proto__ === Hero.prototype,
+// );
+
+// console.log(
+//     'Hero.prototype.__proto__ === Object.prototype ',
+//     Hero.prototype.__proto__ === Object.prototype,
+// );
